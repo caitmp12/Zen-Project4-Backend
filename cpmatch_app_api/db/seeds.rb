3a575ddf9862aaa1e564fb2ad911729ad3bb3aab
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-08 20:43:35 -0500

    seeded data

--- cpmatch_app_api/db/seeds.rb
+++ cpmatch_app_api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create([
+    { title: "Circe", blurb: "Greek myth retelling of world's first witch", genre: "Historical Fantasy", username: "AthenaWarriorPrincess", contact: "@athena_warrior"},
+    { title: "Apron Anxiety", blurb: "The true story of a messy life, in and out of the kitchen", genre: "Memoir", username: "CookingWithWords", contact: "apron_on@gmail.com"},
+    { title: "Mexican Gothic", blurb: "Dark tale about white people doing messed up shit in Mexico in the 20th century", genre: "Horror", username: "writer4865", contact: "silviaMG@gmail.com"},
+    { title: "Beach Read", blurb: "He writes boring but important literature; she writes warm and fuzzies only. They want to hate each other, but instead fall in love.", genre: "Romance", username: "HappyEndingsOnly", contact: "@happily_eva_afta"},
+    { title: "Daisy Jones and the Six", blurb: "Drugs, love, and rocknroll.", genre: "Fiction", username: "StevieFan1810", contact: "@sunshine_kid"},
+    { title: "An Ember in the Ashes", blurb: "He's a soldier looking to get out. She's a terrified rebel trying to break in.", genre: "YA Fantasy", username: "JinnCatcher", contact: "sabaa@sabaatahir.com"}                    
+])
+
+
